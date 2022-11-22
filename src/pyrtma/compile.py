9d30a3c54bc31ab9010d2aad63c27b0dfb35a765
Tyler Madonna <tjm159@pitt.edu>
2022-11-22 09:59:15 -0500

    Fixed lsb string reference in compile

--- src/pyrtma/compile.py
+++ src/pyrtma/compile.py
@@ -309,15 +309,21 @@ def compile(include_files: List, out_filename: Optional[str]):
 if __name__ == "__main__":
     import argparse
 
-    parser = argparse.ArgumentParser(
-        description="LabSwitchBoard Message Definition Compiler."
-    )
+    parser = argparse.ArgumentParser(description="pyrtma Message Definition Compiler.")
 
     parser.add_argument(
-        "-i", "-I", "--include", nargs="*", dest="include_files", help="Files to parse",
+        "-i",
+        "-I",
+        "--include",
+        nargs="*",
+        dest="include_files",
+        help="Files to parse",
     )
     parser.add_argument(
-        "-o", "--out", dest="output_file", help="Output python file",
+        "-o",
+        "--out",
+        dest="output_file",
+        help="Output python file",
     )
     args = parser.parse_args()
     compile(args.include_files, args.output_file)
