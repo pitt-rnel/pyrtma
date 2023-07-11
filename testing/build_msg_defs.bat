d36815021a8544625b833e5198cea76a9a3182ca
David Weir <dmw109@pitt.edu>
2023-07-11 15:07:07 -0400

    Updated build defs script

--- testing/build_msg_defs.bat
+++ testing/build_msg_defs.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 REM pyrtma msg_defs.py compiling
-python -m pyrtma.compile -i climber_config.h -o ./msg_defs/test_defs --c --py --js --mat
+python -m pyrtma.compile -i defs/climber_config.yaml --c --py --js --mat
