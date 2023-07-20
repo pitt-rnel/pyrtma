1c722f7b7c028ffdd585d17bad736bf41a9b2a7d
David Weir <dmw109@pitt.edu>
2023-07-20 16:39:26 -0400

    Added function for random message data for testing.

--- src/pyrtma/_core.py
+++ src/pyrtma/_core.py
@@ -3,10 +3,12 @@
 Core RTMA message definitions and functions"""
 import ctypes
 import json
+import random
+import string
 
 from dataclasses import dataclass
 from collections import ChainMap
-from typing import Type, ClassVar, Optional, Any, Dict, ChainMap
+from typing import Type, ClassVar, Optional, Any, Dict, ChainMap, List
 
 from .constants import *
 
@@ -48,6 +50,126 @@ def _create_ftype_map(obj: "MessageData"):
         "_ftype_map",
         {k: v for k, v in super(MessageData, obj).__getattribute__("_fields_")},
     )
+
+
+def _random_str(length) -> str:
+    return "".join(random.choice(string.printable) for _ in range(length))
+
+
+def _random_int_array(length: int, min: int = 0, max: int = 9) -> List[int]:
+    return [random.randint(min, max) for _ in range(length)]
+
+
+def _random_float_array(length) -> List[float]:
+    return [random.random() for _ in range(length)]
+
+
+def _random_byte_array(length) -> bytes:
+    return bytes([random.randint(0, 255) for _ in range(length)])
+
+
+def is_equal(obj: ctypes.Structure, other: ctypes.Structure) -> bool:
+    if type(obj) != type(other):
+        raise TypeError("Can not compare two different struct types.")
+
+    for name, ftype in obj._fields_:
+        if issubclass(ftype, ctypes.Structure):
+            if not is_equal(getattr(obj, name), getattr(other, name)):
+                return False
+        elif issubclass(ftype, ctypes.Array):
+            length = ftype._length_
+            etype = ftype._type_
+            for i in range(length):
+                if issubclass(etype, ctypes.Structure):
+                    if not is_equal(getattr(obj, name)[i], getattr(other, name)[i]):
+                        return False
+                elif etype is ctypes.c_char:
+                    if getattr(obj, name) != getattr(other, name):
+                        return False
+                else:
+                    if getattr(obj, name)[i] != getattr(other, name)[i]:
+                        return False
+        else:
+            if getattr(obj, name) != getattr(other, name):
+                return False
+
+    return True
+
+
+def _random_struct(obj):
+    for name, ftype in obj._fields_:
+        if issubclass(ftype, ctypes.Structure):
+            setattr(obj, name, _random_struct(getattr(obj, name)))
+        elif issubclass(ftype, ctypes.Array):
+            length = ftype._length_
+            etype = ftype._type_
+            if issubclass(etype, ctypes.Structure):
+                for i in range(length):
+                    getattr(obj, name)[i] = _random_struct(getattr(obj, name)[i])
+            elif etype is ctypes.c_char:
+                setattr(obj, name, _random_str(length))
+            elif etype is ctypes.c_ubyte:
+                getattr(obj, name)[:] = _random_byte_array(length)
+            elif etype is ctypes.c_int8:
+                getattr(obj, name)[:] = _random_int_array(
+                    length, min=-(2**7), max=2**7 - 1
+                )
+            elif etype is ctypes.c_uint8:
+                getattr(obj, name)[:] = _random_int_array(length, min=0, max=2**8 - 1)
+            elif etype in (ctypes.c_short, ctypes.c_int16):
+                getattr(obj, name)[:] = _random_int_array(
+                    length, min=-(2**15), max=2**15 - 1
+                )
+            elif etype in (ctypes.c_ushort, ctypes.c_uint16):
+                getattr(obj, name)[:] = _random_int_array(
+                    length, min=0, max=2**16 - 1
+                )
+            elif etype in (ctypes.c_int, ctypes.c_long, ctypes.c_int32):
+                getattr(obj, name)[:] = _random_int_array(
+                    length, min=-(2**31), max=2**31 - 1
+                )
+            elif etype in (ctypes.c_uint, ctypes.c_ulong, ctypes.c_uint32):
+                getattr(obj, name)[:] = _random_int_array(
+                    length, min=0, max=2**32 - 1
+                )
+            elif etype in (ctypes.c_longlong, ctypes.c_int64):
+                getattr(obj, name)[:] = _random_int_array(
+                    length, min=-(2**63), max=2**63 - 1
+                )
+            elif etype in (ctypes.c_ulonglong, ctypes.c_uint64):
+                getattr(obj, name)[:] = _random_int_array(
+                    length, min=0, max=2**64 - 1
+                )
+            elif etype is ctypes.c_float:
+                getattr(obj, name)[:] = _random_float_array(length)
+            elif etype is ctypes.c_double:
+                getattr(obj, name)[:] = _random_float_array(length)
+        elif ftype is ctypes.c_char:
+            setattr(obj, name, _random_str(1))
+        elif ftype is ctypes.c_ubyte:
+            setattr(obj, name, random.randint(0, 255))
+        elif ftype is ctypes.c_int8:
+            setattr(obj, name, random.randint(-(2**7), 2**7 - 1))
+        elif ftype is ctypes.c_uint8:
+            setattr(obj, name, random.randint(0, 2**8 - 1))
+        elif ftype in (ctypes.c_short, ctypes.c_int16):
+            setattr(obj, name, random.randint(-(2**15), 2**15 - 1))
+        elif ftype in (ctypes.c_ushort, ctypes.c_uint16):
+            setattr(obj, name, random.randint(0, 2**16 - 1))
+        elif ftype in (ctypes.c_int, ctypes.c_long, ctypes.c_int32):
+            setattr(obj, name, random.randint(-(2**31), 2**31 - 1))
+        elif ftype in (ctypes.c_uint, ctypes.c_ulong, ctypes.c_uint32):
+            setattr(obj, name, random.randint(0, 2**32 - 1))
+        elif ftype in (ctypes.c_longlong, ctypes.c_int64):
+            setattr(obj, name, random.randint(-(2**63), 2**63 - 1))
+        elif ftype in (ctypes.c_ulonglong, ctypes.c_uint64):
+            setattr(obj, name, random.randint(0, 2**64 - 1))
+        elif ftype is ctypes.c_float:
+            setattr(obj, name, random.random())
+        elif ftype is ctypes.c_double:
+            setattr(obj, name, random.random())
+
+    return obj
 
 
 def _json_decode(obj, data):
@@ -104,6 +226,11 @@ class MessageData(ctypes.Structure):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         _create_ftype_map(self)
+
+    @classmethod
+    def from_random(cls):
+        obj = _random_struct(cls())
+        return obj
 
     @classmethod
     def from_buffer(cls, source, offset=0):
