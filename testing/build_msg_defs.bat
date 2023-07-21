7c81cdbd348c6470a0dfb11abf7ac5cae8a5a585
David Weir <dmw109@pitt.edu>
2023-07-21 09:23:41 -0400

    Reorganized test files

--- testing/build_msg_defs.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-REM pyrtma msg_defs.py compiling
-python -m pyrtma.compile -i defs/climber_config.yaml --c --py --js --mat
---
+++ tests/build_test_defs.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+REM pyrtma msg_defs.py compiling
+python -m pyrtma.compile -i ./test_msg_defs/climber_config.yaml --c --py --js --mat
