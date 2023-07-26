fc6ad25f1e24f075195404ebe33deb9175b52ded
David Weir <dmw109@pitt.edu>
2023-07-26 13:54:27 -0400

    Update script to build test defs to only include python

--- tests/build_test_defs.bat
+++ tests/build_test_defs.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 REM pyrtma msg_defs.py compiling
-python -m pyrtma.compile -i ./test_msg_defs/test_defs.yaml --c --py --js --mat
+python -m pyrtma.compile -i ./test_msg_defs/test_defs.yaml --py -o ./test_msg_defs/
