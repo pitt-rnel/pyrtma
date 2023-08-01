3c4a8f51fc5f072d51975bca1a1a63178ac7efc1
David Weir <dmw109@pitt.edu>
2023-08-01 11:58:57 -0400

    Update test defs build script

--- tests/build_test_defs.bat
+++ tests/build_test_defs.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 REM pyrtma msg_defs.py compiling
-python -m pyrtma.compile -i ./test_msg_defs/test_defs.yaml --py -o ./test_msg_defs/
+python -m pyrtma.compile -i ./test_msg_defs/test_defs.yaml --py --mat -o ./test_msg_defs/
