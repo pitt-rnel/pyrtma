f742a8c79bd3c42175935dcb403c8388416a1e68
David Weir <dmw109@pitt.edu>
2023-08-07 16:36:10 -0400

    Reformat with black.

--- src/pyrtma/utils/random.py
+++ src/pyrtma/utils/random.py
@@ -36,33 +36,33 @@ def _random_struct(obj):
                 getattr(obj, name)[:] = _random_byte_array(length)
             elif etype is ctypes.c_int8:
                 getattr(obj, name)[:] = _random_int_array(
-                    length, min=-(2 ** 7), max=2 ** 7 - 1
+                    length, min=-(2**7), max=2**7 - 1
                 )
             elif etype is ctypes.c_uint8:
-                getattr(obj, name)[:] = _random_int_array(length, min=0, max=2 ** 8 - 1)
+                getattr(obj, name)[:] = _random_int_array(length, min=0, max=2**8 - 1)
             elif etype in (ctypes.c_short, ctypes.c_int16):
                 getattr(obj, name)[:] = _random_int_array(
-                    length, min=-(2 ** 15), max=2 ** 15 - 1
+                    length, min=-(2**15), max=2**15 - 1
                 )
             elif etype in (ctypes.c_ushort, ctypes.c_uint16):
                 getattr(obj, name)[:] = _random_int_array(
-                    length, min=0, max=2 ** 16 - 1
+                    length, min=0, max=2**16 - 1
                 )
             elif etype in (ctypes.c_int, ctypes.c_long, ctypes.c_int32):
                 getattr(obj, name)[:] = _random_int_array(
-                    length, min=-(2 ** 31), max=2 ** 31 - 1
+                    length, min=-(2**31), max=2**31 - 1
                 )
             elif etype in (ctypes.c_uint, ctypes.c_ulong, ctypes.c_uint32):
                 getattr(obj, name)[:] = _random_int_array(
-                    length, min=0, max=2 ** 32 - 1
+                    length, min=0, max=2**32 - 1
                 )
             elif etype in (ctypes.c_longlong, ctypes.c_int64):
                 getattr(obj, name)[:] = _random_int_array(
-                    length, min=-(2 ** 63), max=2 ** 63 - 1
+                    length, min=-(2**63), max=2**63 - 1
                 )
             elif etype in (ctypes.c_ulonglong, ctypes.c_uint64):
                 getattr(obj, name)[:] = _random_int_array(
-                    length, min=0, max=2 ** 64 - 1
+                    length, min=0, max=2**64 - 1
                 )
             elif etype is ctypes.c_float:
                 getattr(obj, name)[:] = _random_float_array(length)
@@ -73,21 +73,21 @@ def _random_struct(obj):
         elif ftype is ctypes.c_ubyte:
             setattr(obj, name, random.randint(0, 255))
         elif ftype is ctypes.c_int8:
-            setattr(obj, name, random.randint(-(2 ** 7), 2 ** 7 - 1))
+            setattr(obj, name, random.randint(-(2**7), 2**7 - 1))
         elif ftype is ctypes.c_uint8:
-            setattr(obj, name, random.randint(0, 2 ** 8 - 1))
+            setattr(obj, name, random.randint(0, 2**8 - 1))
         elif ftype in (ctypes.c_short, ctypes.c_int16):
-            setattr(obj, name, random.randint(-(2 ** 15), 2 ** 15 - 1))
+            setattr(obj, name, random.randint(-(2**15), 2**15 - 1))
         elif ftype in (ctypes.c_ushort, ctypes.c_uint16):
-            setattr(obj, name, random.randint(0, 2 ** 16 - 1))
+            setattr(obj, name, random.randint(0, 2**16 - 1))
         elif ftype in (ctypes.c_int, ctypes.c_long, ctypes.c_int32):
-            setattr(obj, name, random.randint(-(2 ** 31), 2 ** 31 - 1))
+            setattr(obj, name, random.randint(-(2**31), 2**31 - 1))
         elif ftype in (ctypes.c_uint, ctypes.c_ulong, ctypes.c_uint32):
-            setattr(obj, name, random.randint(0, 2 ** 32 - 1))
+            setattr(obj, name, random.randint(0, 2**32 - 1))
         elif ftype in (ctypes.c_longlong, ctypes.c_int64):
-            setattr(obj, name, random.randint(-(2 ** 63), 2 ** 63 - 1))
+            setattr(obj, name, random.randint(-(2**63), 2**63 - 1))
         elif ftype in (ctypes.c_ulonglong, ctypes.c_uint64):
-            setattr(obj, name, random.randint(0, 2 ** 64 - 1))
+            setattr(obj, name, random.randint(0, 2**64 - 1))
         elif ftype is ctypes.c_float:
             setattr(obj, name, random.random())
         elif ftype is ctypes.c_double:
