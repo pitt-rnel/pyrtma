ee2b7a193de217c70290a3252246ba1d13481065
David Weir <david.weir@pitt.edu>
2024-01-19 09:08:32 -0500

    Store QL data as dicts

--- src/pyrtma/utils/quicklogger_reader.py
+++ src/pyrtma/utils/quicklogger_reader.py
@@ -6,28 +6,14 @@ import importlib
 import pyrtma
 import base64
 import json
+import copy
 
-from typing import List, Union
-from ..message import MessageData, MessageHeader
+from typing import List, Union, Tuple, Generator, Dict, Any
+from ..message import RTMAJSONEncoder, MessageHeader
 
 
-class UnknownMessageData:
-    def __init__(self, raw: bytes):
-        self._data = raw
-
-    @property
-    def data(self) -> bytes:
-        return self._data
-
-    def to_b64(self) -> str:
-        return base64.b64encode(self._data).decode()
-
-    def to_json(self, minify=True, **kwargs) -> str:
-        d = dict(_raw=self.to_b64())
-        if minify:
-            return json.dumps(d, separators=(",", ":"))
-        else:
-            return json.dumps(d, indent=2)
+def create_unknown(raw: bytes):
+    return dict(_unknown=base64.b64encode(raw).decode())
 
 
 class QLFileHeader(ctypes.Structure):
@@ -46,74 +32,103 @@ class QLReader:
         self.file_path = None
         self.defs_path = None
         self.file_header = QLFileHeader()
-        self.headers: List[MessageHeader] = []
-        self.data: List[Union[MessageData, UnknownMessageData]] = []
+        self.headers: List[Dict[str, Any]] = []
+        self.offsets: List[int] = []
+        self.data: List[Dict[str, Any]] = []
 
     def clear(self):
         self.file_path = None
         self.defs_path = None
         self.file_header = QLFileHeader()
         self.headers.clear()
+        self.offsets.clear()
         self.data.clear()
 
-    def load(self, binfile: Union[str, os.PathLike], msgdefs: Union[str, os.PathLike]):
+    def load(
+        self,
+        binfile: Union[str, os.PathLike],
+        msgdefs: Union[str, os.PathLike],
+        skip_unknown: bool = True,
+    ):
         self.defs_path = pathlib.Path(msgdefs)
         self.file_path = pathlib.Path(binfile)
 
         # Import the message definitions
         base = self.defs_path.absolute().parent
         fname = self.defs_path.stem
+
+        # Copy the current message def context before importing
+        ctx = copy.deepcopy(pyrtma.msg_defs)
+
         sys.path.insert(0, (str(base.absolute())))
         importlib.import_module(fname)
 
-        headers = []
-        data: List[Union[MessageData, UnknownMessageData]] = []
-        with open(self.file_path, "rb") as f:
-            # Parse binary file header
-            file_header = QLFileHeader.from_buffer_copy(
-                f.read(ctypes.sizeof(QLFileHeader))
-            )
-            msg_header_size = file_header.message_header_size
+        try:
+            headers: List[Dict[str, Any]] = []
+            data: List[Dict[str, Any]] = []
 
-            # Extract the message headers
-            for _ in range(file_header.num_messages):
-                raw = f.read(msg_header_size)
-                headers.append(MessageHeader.from_buffer_copy(raw))
+            with open(self.file_path, "rb") as f:
+                # Parse binary file header
+                file_header = QLFileHeader.from_buffer_copy(
+                    f.read(ctypes.sizeof(QLFileHeader))
+                )
+                msg_header_size = file_header.message_header_size
 
-            # Extract the message data offsets for each message
-            offset_size = file_header.data_block_offset_size
-            offsets = (ctypes.c_uint32 * file_header.num_messages).from_buffer_copy(
-                f.read(offset_size * file_header.num_messages)
-            )
+                # Extract the message headers
+                for _ in range(file_header.num_messages):
+                    raw = f.read(msg_header_size)
+                    headers.append(MessageHeader.from_buffer_copy(raw).to_dict())
 
-            # Read the entire data block remaining
-            d = f.read()
+                # Extract the message data offsets for each message
+                offset_size = file_header.data_block_offset_size
+                offsets = (ctypes.c_uint32 * file_header.num_messages).from_buffer_copy(
+                    f.read(offset_size * file_header.num_messages)
+                )
+                self.offsets = list(map(int, offsets))
 
-            # Extract the message data for each message
-            for n, offset in enumerate(offsets):
-                header = headers[n]
-                msg_cls = pyrtma.msg_defs.get(header.msg_type)
-                raw_bytes = d[offset : offset + header.num_data_bytes]
+                # Read the entire data block remaining
+                d = f.read()
 
-                if msg_cls is None:
-                    print(f"Unknown message definition: MT={header.msg_type}")
-                    msg = UnknownMessageData(raw_bytes)
+                unknown: List[int] = []
+                # Extract the message data for each message
+                for n, offset in enumerate(offsets):
+                    header = headers[n]
+                    msg_cls = pyrtma.msg_defs.get(header["msg_type"])
+                    raw_bytes = d[offset : offset + header["num_data_bytes"]]
 
-                else:
-                    if msg_cls.type_size != header.num_data_bytes:
+                    if msg_cls is None:
+                        print(f"Unknown message definition: MT={header['msg_type']}")
+                        msg = create_unknown(raw_bytes)
+                        unknown.append(n)
+
+                    elif msg_cls.type_size != header["num_data_bytes"]:
                         print(
-                            f"Warning: Message header indicates a message data size ({header.num_data_bytes}) that does not match the expected size of message type {msg_cls.type_name} ({msg_cls.type_size}). Message definitions may be out of sync."
+                            f"Warning: Message header indicates a message data size ({header['num_data_bytes']}) that does not match the expected size of message type {msg_cls.type_name} ({msg_cls.type_size}). Message definitions may be out of sync."
                         )
-                        msg = UnknownMessageData(raw_bytes)
+                        msg = create_unknown(raw_bytes)
+                        unknown.append(n)
                     else:
-                        msg = msg_cls.from_buffer_copy(raw_bytes)
+                        msg = msg_cls.from_buffer_copy(raw_bytes).to_dict()
 
-                data.append(msg)
+                    data.append(msg)
+
+        finally:
+            # Restore the orignal message def context
+            pyrtma.msg_defs = ctx
 
         # Store the results in the object
         self.file_header = file_header
-        self.headers = headers
-        self.data = data
+        self.headers = []
+        self.data = []
+        if skip_unknown:
+            for n, (h, d) in enumerate(zip(headers, data, strict=True)):
+                if n not in unknown:
+                    self.headers.append(h)
+                    self.data.append(d)
+
+        else:
+            self.headers = headers
+            self.data = data
 
     def export_json(self, file: Union[str, os.PathLike]):
         save_path = pathlib.Path(file)
@@ -132,13 +147,28 @@ class QLReader:
 
                 # Header json object
                 f.write('"header":')
-                f.write(header.to_json(minify=True))
+                f.write(json.dumps(header, cls=RTMAJSONEncoder, separators=(",", ":")))
 
                 f.write(",")
 
                 # Data json object
                 f.write('"data":')
-                f.write(data.to_json(minify=True))
+                f.write(json.dumps(data, cls=RTMAJSONEncoder, separators=(",", ":")))
 
                 # Close json object
                 f.write("}\n")
+
+    def iter_by_mt(
+        self, msg_type: int
+    ) -> Generator[Tuple[Dict[str, Any], Dict[str, Any]], Any, None]:
+        for header, data in zip(self.headers, self.data, strict=True):
+            if header["msg_type"] == msg_type:
+                yield (header, data)
+
+    def iter_by_src(
+        self,
+        mod_id: int,
+    ) -> Generator[Tuple[Dict[str, Any], Dict[str, Any]], Any, None]:
+        for header, data in zip(self.headers, self.data, strict=True):
+            if header["src_mod_id"] == mod_id:
+                yield (header, data)
