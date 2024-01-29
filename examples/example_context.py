dd77aa2eebcea3a041c3f45a7776e0653121d9ec
Jeff Weiss <jmw182@pitt.edu>
2024-01-29 15:13:58 -0500

    Fixed example and added example_context

---
+++ examples/example_context.py
@@ -0,0 +1,88 @@
+import sys
+import pathlib
+
+sys.path.append(str(pathlib.Path(__file__).parent))
+
+import pyrtma
+
+# Import the users compiled message defintions
+import msg_defs.example_messages as md
+
+from typing import cast
+
+
+def publisher(server="127.0.0.1:7111", timecode=False):
+    # Setup Client
+    with pyrtma.client_context(server_name=server, timecode=timecode) as mod:
+        # Build a packet to send
+        msg = md.MDF_PERSON_MESSAGE()
+        msg.name = "Alice"
+        msg.age = 37
+
+        while True:
+            c = input("Hit enter to publish a message. (Q)uit.")
+
+            if c not in ["Q", "q"]:
+                msg.age += 1
+                mod.send_message(msg)
+                print("Sent:")
+                print(msg.to_json())
+            else:
+                mod.send_signal(md.MT_EXIT)
+                print("Goodbye")
+                break
+
+
+def subscriber(server="127.0.0.1:7111", timecode=False):
+    # Setup Client and select the messages to receive
+    with pyrtma.client_context(
+        server_name=server,
+        timecode=timecode,
+        msg_list=[md.MT_PERSON_MESSAGE, md.MT_EXIT],
+    ) as mod:
+        print("Waiting for packets...")
+        while True:
+            try:
+                msg = mod.read_message(timeout=0.200)
+
+                if msg is not None:
+                    if msg.type_id == md.MT_PERSON_MESSAGE:
+                        # optional cast for better type hinting
+                        msg.data = cast(md.MDF_PERSON_MESSAGE, msg.data)
+                        print("Recv:")
+                        print(msg.data.to_json())
+                    elif msg.type_id == md.MT_EXIT:
+                        print("Goodbye.")
+                        break
+            except KeyboardInterrupt:
+                break
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--server", default="127.0.0.1:7111", help="RTMA Message Manager ip address."
+    )
+    parser.add_argument(
+        "-t", "--timecode", action="store_true", help="Use timecode in message header"
+    )
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        "--pub", default=False, action="store_true", help="Run as publisher."
+    )
+    group.add_argument(
+        "--sub", default=False, action="store_true", help="Run as subscriber."
+    )
+
+    args = parser.parse_args()
+
+    if args.pub:
+        print("pyrtma Publisher")
+        publisher(args.server, timecode=args.timecode)
+    elif args.sub:
+        print("pyrtma Subscriber")
+        subscriber(args.server, timecode=args.timecode)
+    else:
+        print("Unknown input")
