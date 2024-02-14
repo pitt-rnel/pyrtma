fb74588632a625cbea87dcc87ece034e9f10bb09
Jeff Weiss <jmw182@pitt.edu>
2024-02-14 10:32:05 -0500

    bump version to 2.1.4

--- src/pyrtma/__version__.py
+++ src/pyrtma/__version__.py
@@ -1,7 +1,7 @@
 __title__ = "pyrtma"
 __description__ = "A Python compiler and client for RTMA/Dragonfly messaging system"
 __url__ = "https://github.com/pitt-rnel/pyrtma"
-__version__ = "2.1.3"
+__version__ = "2.1.4"
 __author__ = "RNEL"
 __author_email__ = ""
 __license__ = "MIT"
