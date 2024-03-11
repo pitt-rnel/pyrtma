47f69be3b5d4ebdb798a04f576ddaffc732fb78c
Jeff Weiss <jmw182@pitt.edu>
2024-03-11 16:35:21 -0400

    bump version to 2.2.2

---
+++ build_all_msg_defs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python -m pyrtma.compile -i src/pyrtma/core_defs/core_defs.yaml -o src/pyrtma/ --py
+python -m pyrtma.compile -i examples/msg_defs/example_messages.yaml -o examples/msg_defs/ --py
+python -m pyrtma.compile -i tests/test_msg_defs/test_defs.yaml -o tests/test_msg_defs/ --py --mat
