bcffadc99c1321cac46a66a249c99eb6f478183a
Jeff Weiss <jmw182@pitt.edu>
2024-11-22 14:49:43 -0500

    Add test to catch message defs being compiled with out-of-date pyrtma version

---
+++ tests/test_message_version.py
@@ -0,0 +1,21 @@
+import unittest
+import pyrtma
+import pyrtma.core_defs as cd
+import sys
+from .test_msg_defs import test_defs as td
+
+sys.path.append("..")
+from examples.msg_defs import example_messages as em
+
+
+class TestMessageVersion(unittest.TestCase):
+    """Test that the core defs and example/test defs were built
+    with the same version of pyrtma
+
+    It is easy to miss this step before deployment, so this test will catch it
+    """
+
+    def test_version(self):
+        self.assertEqual(pyrtma.__version__, cd.COMPILED_PYRTMA_VERSION)
+        self.assertEqual(pyrtma.__version__, td.COMPILED_PYRTMA_VERSION)
+        self.assertEqual(pyrtma.__version__, em.COMPILED_PYRTMA_VERSION)
