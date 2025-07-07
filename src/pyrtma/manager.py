c9da9b0444e29f4f109d2656242736fb81134357
Jeff Weiss <jmw182@pitt.edu>
2025-07-07 10:19:32 -0400

    fixed regression: legacy QL w/ DEBUG_TEXT should not crash MM

--- src/pyrtma/manager.py
+++ src/pyrtma/manager.py
@@ -845,7 +845,9 @@ class MessageManager(ClientLike):
             header (MessageHeader): Message header of the incoming message
         """
         msg_type = header.msg_type
-        if msg_type >= 100:
+        if msg_type >= 100 or msg_type == 91:
+            # 91 is legacy MT_DEBUG_TEXT, incompatible with pyrtma but sent by legacy QL
+            # TODO deprecate when switching to new data_logger
             return
 
         core_msg = self.decode_core_message(header)
