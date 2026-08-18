63197a8c1eb4c57e60f30e3c8a65292ee96630da
David Weir <david.weir@pitt.edu>
2026-08-18 13:06:08 -0400

    Add tests for explicit message definitions and dataset configuration

--- tests/test_message_definitions.py
+++ tests/test_message_definitions.py
@@ -3,6 +3,7 @@ import unittest
 
 import pyrtma
 import pyrtma.core_defs as cd
+from pyrtma.data_logger.dataset import Dataset
 from pyrtma.definitions import MessageDefinitions
 from pyrtma.loader import load_message_definitions
 
@@ -30,6 +31,35 @@ class TestMessageDefinitions(unittest.TestCase):
 
         self.assertIsInstance(defs, MessageDefinitions)
         self.assertEqual(defs.message_id_from_name("ACKNOWLEDGE"), cd.MT_ACKNOWLEDGE)
+
+    def test_dataset_uses_explicit_definitions_without_client(self):
+        defs_path = pathlib.Path(__file__).parent / "test_msg_defs" / "test_defs.py"
+        defs = load_message_definitions(defs_path)
+
+        ds = Dataset(
+            name="alias",
+            save_path=".",
+            filename="alias",
+            formatter="json",
+            msg_types=[6000],
+            create_client=False,
+            definitions=defs,
+        )
+
+        self.assertEqual(ds.msg_names, ("ALIAS_TEST",))
+
+    def test_dataset_config_processing_uses_explicit_definitions(self):
+        defs_path = pathlib.Path(__file__).parent / "test_msg_defs" / "test_defs.py"
+        defs = load_message_definitions(defs_path)
+
+        msg = cd.MDF_DATA_LOGGER_CONFIG()
+        msg.num_datasets = 1
+        msg.datasets[0].name = "alias"
+        msg.datasets[0].msg_types[0] = 6000
+
+        config = Dataset.process_data_logger_config_msg(msg, definitions=defs)
+
+        self.assertEqual(config["datasets"][0]["msg_types"], ["ALIAS_TEST"])
 
 
 if __name__ == "__main__":
