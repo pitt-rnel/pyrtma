418ab11f607dbe6cda09892c4f72c2d0f1406634
Jeff Weiss <jmw182@pitt.edu>
2026-08-20 16:38:21 -0400

    remove unused __version__ import

--- src/pyrtma/compilers/python.py
+++ src/pyrtma/compilers/python.py
@@ -224,7 +224,7 @@ class PyDefCompiler:
         s = """\
         import ctypes
 
-        from pyrtma.__version__ import check_compiled_version, __version__
+        from pyrtma.__version__ import check_compiled_version
         from pyrtma.definitions import MessageDefinitions, build_message_definitions
         from typing import ClassVar
 
